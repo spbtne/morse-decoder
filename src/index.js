b8e777ac54d5554b058e9dfe12b119584de5f124
spbtne <69213046+spbtne@users.noreply.github.com>
2020-10-20 15:30:28 +0300

    feat: add solution

--- src/index.js
+++ src/index.js
@@ -38,7 +38,51 @@ const MORSE_TABLE = {
 };
 
 function decode(expr) {
-    // write your solution here
+    let space = '**********';
+      let dot = '10';
+      let dash = '11';
+      let exprArr = expr.split(space);
+       let answer ='';
+      for (let j = 0; j < exprArr.length; j += 1)
+      {
+      let wordsArr = exprArr[j].match(/.{1,10}/g);
+     
+    
+      for (let i = 0; i < wordsArr.length; i += 1) {
+       let oneWordsCode = wordsArr[i].split('');
+       
+        for (let y = 0 ; y < 10 ; y += 1)
+          {
+            if (oneWordsCode[y] === '0')
+            {
+            oneWordsCode.splice(y,1);
+            y -=1;
+            }
+            else {
+              wordsArr[i] = oneWordsCode.join('');
+              break
+              }
+          }    
+    }
+     let result = wordsArr.reduce(function(word, item, index, array){
+        let letterArray = item.match(/.{1,2}/g);
+        let wordsMorseCode = letterArray.reduce(function(morseCode, i){
+           if (i === '10') {
+             morseCode +='.';
+           }if (i === '11') {
+             morseCode +='-'
+           }
+           return morseCode;
+         },'')
+        word += MORSE_TABLE[wordsMorseCode];
+        return word;
+      },'')
+     
+     answer +=' ' + result;
+        }
+        let rslt = answer.split('');
+        rslt.splice(0,1);
+        return rslt.join('');
 }
 
 module.exports = {
